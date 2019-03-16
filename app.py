ff321480a9382e435f9e04b7097423176f5092d5
Tyler Cobb <tyjcobb94@gmail.com>
2019-03-16 13:39:29 -0500

    Insert/Retrieve only a users own weights

--- app.py
+++ app.py
@@ -11,15 +11,9 @@ FitbioDB = MongoClient[os.environ["FITBIO_DB"]]
 WeightCollection = FitbioDB[os.environ["WEIGHT_COLLECTION"]]
 
 
-@app.route("/weight/<string:user_id>", methods=["GET"])
-def get_weight(user_id):
-    # TODO - user validation
-    print(request.environ)
-    print(list(request.environ.keys()))
-    print(request.environ["event"])
-    print(request.environ["event"]["requestContext"])
-    print(request.environ["event"]["requestContext"]["identity"])
-    print(request.environ["event"]["requestContext"]["identity"]["cognitoIdentityId"])
+@app.route("/weight", methods=["GET"])
+def get_weight():
+    user_id = request.environ["event"]["requestContext"]["identity"]["cognitoIdentityId"]
     res = WeightCollection.find_one({"user_id": user_id}) or {}
 
     # ObjectId not serializable, there is probably a better way to do this
@@ -31,10 +25,10 @@ def get_weight(user_id):
 
 @app.route("/weight", methods=["POST"])
 def insert_weight():
-    # TODO - Input validation
     # TODO - Sort by date?
     # TODO - Consider max doc size of 16MB if we are storing all weights for someone in single doc
-    existing = WeightCollection.find_one({"user_id": request.json["user_id"]}) or {}
+    user_id = request.environ["event"]["requestContext"]["identity"]["cognitoIdentityId"]
+    existing = WeightCollection.find_one({"user_id": user_id}) or {}
     existing_weights = existing["weights"] if "weights" in existing else []
     new_weight_dates = [w["date"] for w in request.json["weights"]]
     merged_weights = request.json["weights"]
@@ -43,7 +37,7 @@ def insert_weight():
         merged_weights.append(old_weight)
 
     WeightCollection.update_one(
-        {"user_id": request.json["user_id"]},
+        {"user_id": user_id},
         {"$set": {"weights": merged_weights}},
         upsert=True
     )
